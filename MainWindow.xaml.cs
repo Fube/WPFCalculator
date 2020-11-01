a249920bfd4e3c0feae0e4bd248c78c9b892b752
Fube <fubeitch@gmail.com>
2020-11-01 15:29:08 -0500

    I guess it's finished? Was kinda soft ngl...

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,7 +1,11 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Reflection;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,9 +24,138 @@ namespace WPFCalculator
     /// </summary>
     public partial class MainWindow : Window
     {
+        // Class level var = static var
+        private static double?
+                lastNumber = null,
+                result = null; // What is the point of result? Everything works without it
+
+        private SelectedOperator? selectedOperator = null;
+
+        private enum SelectedOperator
+        {
+            Addition,
+            Subtraction,
+            Multiplication,
+            Division
+        }
+
         public MainWindow()
         {
             InitializeComponent();
+
+
+            // You know what would make this easier? 
+            // CSS selectors.
+            // WPF sucks. Change my mind
+
+            foreach (var elem in myGrid.Children)
+            {
+
+                if(elem is Button button)
+                {
+
+
+                    button.Click += HandleClick( button.Content.ToString() );
+                }
+            }
+        }
+
+        private RoutedEventHandler HandleClick(string content)
+        {
+            const string IS_NUMBER = @"^\d+$";
+            const string IS_OPERATION = @"^(\+|\-|\*|\/)$";
+            const string IS_SPECIAL_OPERATION = @"^(\+\/\-|\%|AC|=|\.)$";
+
+            RoutedEventHandler REH = (e, s) => Trace.WriteLine("Unknown");
+
+            if (Regex.IsMatch(content, IS_NUMBER))
+            {
+                REH = (e, s) => HandleNumber(content);
+            }
+            else if(Regex.IsMatch(content, IS_OPERATION))
+            {
+                REH = (e, s) => HandleOperation(content);
+            }
+            else if(Regex.IsMatch(content, IS_SPECIAL_OPERATION))
+            {
+                REH = (e, s) => HandleSpecialOperation(content);
+            }
+
+            return REH;
+        }
+
+        private void HandleOperation(string operation)
+        {
+
+            if(selectedOperator != null) // Throw error if user changes the opeartion half way through
+            {
+                throw new Exception("Wtf dude");
+            }
+
+            switch (operation)
+            {
+                case "+": selectedOperator = SelectedOperator.Addition;         break;
+                case "-": selectedOperator = SelectedOperator.Subtraction;      break;
+                case "*": selectedOperator = SelectedOperator.Multiplication;   break;
+                case "/": selectedOperator = SelectedOperator.Division;         break;
+            }
+        }
+
+        private void HandleNumber(string number)
+        {
+
+            if (selectedOperator != null && lastNumber == null) // Second number flow
+            {
+                lastNumber = double.Parse(Result.Content + "");
+                Result.Content = number;
+            }
+            else // Normal flow
+            {
+                Result.Content = Result.Content.ToString().Equals("0") ? number : Result.Content + number;
+            }
+        }
+
+        private void HandleSpecialOperation(string operation)
+        {
+
+            switch (operation)
+            {
+                case "=":
+                    {
+                        try
+                        {
+                            Result.Content = DoOperation(selectedOperator, double.Parse(lastNumber + ""), double.Parse(Result.Content + ""));
+                        }
+                        catch(Exception ex) when (ex is ArithmeticException || ex is DivideByZeroException) // Division by zero is legal apparently. It just returns Infinity...
+                        {
+                            Result.Content = "That is mathematically impossible";
+                        }
+                        Reset();
+                        break;
+                    }
+                case "AC":  Reset(); Result.Content = 0; break;
+                case "+/-": Result.Content = double.Parse(Result.Content + "") * -1; break;
+                case "%":   selectedOperator = SelectedOperator.Addition; Result.Content = double.Parse(Result.Content + "") * 0.01; break;
+                case ".": if (Result.Content.ToString().Contains(".")) return; else Result.Content += "."; break;
+            }
+        }
+
+        private double? DoOperation(SelectedOperator? SO, double o, double o1)
+        {
+            switch (SO)
+            {
+                case SelectedOperator.Addition: return MathService.Add(o, o1);
+                case SelectedOperator.Subtraction: return MathService.Subtract(o, o1);
+                case SelectedOperator.Multiplication: return MathService.Multiply(o, o1);
+                case SelectedOperator.Division: return MathService.Divide(o, o1);
+                default: return null;
+            }
+        }
+
+        private void Reset() // Note that reset and "AC" are not the same
+        {
+            lastNumber = result = null;
+            selectedOperator = null;
         }
     }
 }
---
+++ MathService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFCalculator
+{
+    static class MathService
+    {
+
+        public static double Add(double o, double o1) => o + o1;
+        public static double Subtract(double o, double o1) => o - o1;
+        public static double Multiply(double o, double o1) => o * o1;
+        public static double Divide(double o, double o1){
+
+            if (o1 == 0) // Have to force the expetion cause it refuses to throw it by itself
+                throw new DivideByZeroException("Can't divide by zero");
+
+            return o / o1;
+        }
+    }
+}
